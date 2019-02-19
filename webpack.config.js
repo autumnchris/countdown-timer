1d775048a7175dca1a4715ac60f45738907f7985
Autumn <autumncbullard@gmail.com>
2019-02-18 22:33:31 -0500

    Install and set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
