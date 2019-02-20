440011dac194e5e9da759248229568501ec20804
Autumn <autumncbullard@gmail.com>
2019-02-19 22:49:58 -0500

    Set up starter code for app.js

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="body"></div>
+    );
+  }
+}
