9beb33bd6c59e954169a580e8108dda057d4d651
Autumn <autumncbullard@gmail.com>
2019-02-19 22:55:43 -0500

    Add markup for header content

--- src/components/app.js
+++ src/components/app.js
@@ -9,8 +9,16 @@ export default class App extends Component {
   render() {
     return (
       <div className="body">
-          {/* FOOTER */}
-          <footer>Coded by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a></footer>
+        {/* HEADER */}
+        <header>
+          <h1>Countdown Timer</h1>
+          <div className="button-group">
+            <button type="button" className="clear">Clear</button>
+            <button type="button" className="settings">Settings</button>
+          </div>
+        </header>
+        {/* FOOTER */}
+        <footer>Coded by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a></footer>
       </div>
     );
   }
