a445e55bbb28782569dd12ab2782ad26ab0d7af3
autumnchris <autumncbullard@gmail.com>
2020-07-25 17:51:14 -0400

    Use the ES6 import statement where needed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-
-require('./stylesheets/style.scss');
+import './stylesheets/style.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
