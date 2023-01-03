185f14290d2fd30f5ee2048ba474e4821ade8746
autumnchris <autumncbullard@gmail.com>
2023-01-03 14:23:04 -0500

    Add aria-hidden attribute to icons

--- src/components/InfoMessage.js
+++ src/components/InfoMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoMessage = ({ countdownInfoMessage }) => {
 
-  return <p className="message info-message"><span className="fa fa-info-circle fa-lg fa-fw"></span> {countdownInfoMessage}</p>;
+  return <p className="message info-message"><span className="fa fa-info-circle fa-lg fa-fw" aria-hidden="true"></span> {countdownInfoMessage}</p>;
 }
 
 export default InfoMessage;
