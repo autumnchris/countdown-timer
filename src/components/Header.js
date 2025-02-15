4da71bcb5302682ed2c6459e5ddd5b8da5b56bd1
autumnchris <autumncbullard@gmail.com>
2025-02-14 19:36:12 -0500

    Improve semantic HTML

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ const Header = ({ clearCountdown, setModalVisibility }) => {
   return (
     <header>
       <h1 className="header-item">Countdown Timer</h1>
-      <div className="button-group header-item">
-        <button type="button" className="button header-button clear" onClick={() => clearCountdown()}>Clear</button>
-        <button type="button" className="button header-button settings" onClick={() => setModalVisibility(true)}>Settings</button>
-      </div>
+      <aside>
+        <div className="button-group header-item">
+          <button type="button" className="button header-button clear" onClick={() => clearCountdown()}>Clear</button>
+          <button type="button" className="button header-button settings" onClick={() => setModalVisibility(true)}>Settings</button>
+        </div>
+      </aside>
     </header>
   );
 }
